c570e2b5bd3e471b69593b6a46fd39576b1df7a7
Goutham990 <kgouthamnayak61039@gmail.com>
2024-06-14 18:46:20 +0530

    Portfolio Website new

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
   return (
     
       <nav className="navbar">
-        <a href="https://goutham990.github.io/GouthamWeb/">
+        <a href="/">
         <img src={logo} alt="Logo" className='logo' />
         </a>
        
